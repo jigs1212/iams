57847b457cd8bb0cb6113ca08ce6c1ba84fd9400
Jigar Lodaya <jigar.lodaya12@gmail.com>
2016-10-06 01:47:08 +0530

    Php unit upgraded to 5.0 and test case edited.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,48 @@
-@extends('theme.layout.master')
-@section('after-styles-end')
-@section ('title', config('settings.project-name').'|Welcome')
-@endsection
+<html>
+    <head>
+        <title>Laravel</title>
 
-@section('content')
-@endsection
+        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-@section('after-scripts-end')
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
 
-@endsection
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+                margin-bottom: 40px;
+            }
+
+            .quote {
+                font-size: 24px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+                {{-- <div class="quote">{{ Inspiring::quote() }}</div> --}}
+            </div>
+        </div>
+    </body>
+</html>
